433d80b2c8909d8fbf5ff0e3b2801f8f9a5d82d3
Pedro Munhoz <pedro.munhoz@gmail.com>
2022-04-21 18:33:48 -0300

    config file includes prod

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ const configs = {
   development: {
     apiBaseUrl: 'http://localhost:8090',
   },
+  production: {
+    apiBaseUrl: 'http://localhost:8090',
+  },
 };
 
 const nodeEnv = process.env.NODE_ENV;
