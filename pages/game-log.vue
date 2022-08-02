d50b74d9e813a8fd52d8ed9c195db29fcc3d3c8d
Pedro Munhoz <pedro.munhoz@gmail.com>
2022-08-01 23:41:39 -0300

    functional game management

---
+++ pages/game-log.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-container>
+        <v-row>
+          <v-col>Game log</v-col>
+          <v-col>
+            <game-dialog :edit-mode="false" @confirm="create($event)" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-title>
+    <v-data-table
+      :headers="dataTable.headers"
+      :items="players"
+      :options.sync="options"
+      :loading="loading"
+      :server-items-length="playerCount"
+    >
+      <template #item.date="{ item }">
+        {{ formatDate(item.date) }}
+      </template>
+      <template #item.edit="{ item }">
+        <game-dialog
+          :old-date="item.date"
+          :old-home-team="item.homeTeam"
+          :old-away-team="item.awayTeam"
+          :old-location="item.location"
+          edit-mode
+          @confirm="update(item, $event)"
+        />
+      </template>
+      <template #item.delete="{ item }">
+        <v-icon icon @click="deleteItem(item)"> mdi-delete-outline </v-icon>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+import GamesService from '@/services/GamesService';
+import GameDialog from '@/components/GameDialog.vue';
+
+export default {
+  components: {
+    GameDialog,
+  },
+
+  data: () => ({
+    dataTable: {
+      headers: [
+        { text: 'Date', value: 'date' },
+        { text: 'Home Team', value: 'homeTeam' },
+        { text: 'Away Team', value: 'awayTeam' },
+        { text: 'Location', value: 'location' },
+        { text: 'Edit', value: 'edit' },
+        { text: 'Delete', value: 'delete' },
+      ],
+    },
+    players: [],
+    playerCount: 0,
+    info: null,
+    options: {
+      itemsPerPage: 5,
+    },
+    loading: false,
+  }),
+
+  computed: {
+    fields() {
+      return this.players;
+    },
+  },
+
+  watch: {
+    options: {
+      handler() {
+        this.load();
+      },
+      deep: true,
+    },
+  },
+
+  methods: {
+    update(item, event) {
+      item.date = new Date(event.date);
+      item.homeTeam = event.homeTeam;
+      item.awayTeam = event.awayTeam;
+      item.location = event.location;
+      this.updateItem(item);
+    },
+
+    updateItem(item) {
+      const service = new GamesService();
+      service.Update(item).finally((response) => this.load());
+    },
+
+    create(event) {
+      const service = new GamesService();
+      service.Create(event).finally((response) => this.load());
+    },
+
+    deleteItem(item) {
+      const service = new GamesService();
+      service.Delete(item.id).finally((response) => this.load());
+    },
+
+    load() {
+      this.loading = true;
+      const index = this.options.itemsPerPage * (this.options.page - 1);
+      const length = this.options.itemsPerPage;
+
+      const service = new GamesService();
+      service
+        .List(index, length)
+        .then((response) => {
+          this.players = response.data.itens;
+          this.playerCount = response.data.count;
+        })
+        .finally(() => (this.loading = false));
+    },
+
+    formatDate(dateToBeFormatted) {
+      const date = new Date(dateToBeFormatted);
+      return new Intl.DateTimeFormat('default', {
+        dateStyle: 'full',
+        timeStyle: 'short',
+      }).format(date);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ services/GamesService.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const basePath = `${process.env.API_BASE_URL}/api/v1/Games`;
+
+export default class AthletesService {
+  List(index, length) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${basePath}?length=${length}&index=${index}`)
+        .then((response) => resolve(response))
+        .catch((err) => reject(err));
+    });
+  }
+
+  Create(game) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(basePath, game)
+        .then((response) => resolve(response))
+        .catch((err) => reject(err));
+    });
+  }
+
+  Update(game) {
+    return new Promise((resolve, reject) => {
+      axios
+        .put(basePath, game)
+        .then((response) => resolve(response))
+        .catch((err) => reject(err));
+    });
+  }
+
+  Delete(id) {
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(`${basePath}/${id}`)
+        .then((response) => resolve(response))
+        .catch((err) => reject(err));
+    });
+  }
+}
